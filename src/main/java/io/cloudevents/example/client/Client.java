416a194c429b2e2d5b150954423b10888504de24
Yurii Serhiichuk <savik.ne@gmail.com>
2022-10-08 21:23:00 +0300

    Cleanup code

--- src/main/java/io/cloudevents/example/client/Client.java
+++ src/main/java/io/cloudevents/example/client/Client.java
@@ -51,15 +51,15 @@ public class Client {
   /**
    * Say hello to server.
    */
-  public void greet(String name) {
-    logger.info("Will try to greet " + name + " ...");
+  public void greet() {
     CloudEvent request = CloudEvent.newBuilder()
-        .setType("io.cloudevents.example.Requested")
-        .setSource("internal")
+        .setType("io.cloudevents.example.client.RequestSent")
+        .setSource("io.cloudevents.example.client")
         .setSpecVersion("1.0.0")
         .setId("1")
-        .setTextData("Hello")
+        .setTextData("ping")
         .build();
+    logger.info("Sending CloudEvent: " + TextFormat.shortDebugString(request));
     CloudEvent response;
     try {
       response = blockingStub.hello(request);
@@ -67,7 +67,7 @@ public class Client {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
     }
-    logger.info("Greeting: " + TextFormat.shortDebugString(response));
+    logger.info("Received response: " + TextFormat.shortDebugString(response));
   }
 
   /**
@@ -75,24 +75,8 @@ public class Client {
    * greeting. The second argument is the target server.
    */
   public static void main(String[] args) throws Exception {
-    String user = "world";
     // Access a service running on the local machine on port 50051
-    String target = "localhost:53051";
-    // Allow passing in the user and target strings as command line arguments
-    if (args.length > 0) {
-      if ("--help".equals(args[0])) {
-        System.err.println("Usage: [name [target]]");
-        System.err.println("");
-        System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
-        System.err.println("  target  The server to connect to. Defaults to " + target);
-        System.exit(1);
-      }
-      user = args[0];
-    }
-    if (args.length > 1) {
-      target = args[1];
-    }
-
+    String target = "localhost:52051";
     // Create a communication channel to the server, known as a Channel. Channels are thread-safe
     // and reusable. It is common to create channels at the beginning of your application and reuse
     // them until the application shuts down.
@@ -103,7 +87,7 @@ public class Client {
         .build();
     try {
       Client client = new Client(channel);
-      client.greet(user);
+      client.greet();
     } finally {
       // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
       // resources the channel should be shut down when it will no longer be used. If it may be used
